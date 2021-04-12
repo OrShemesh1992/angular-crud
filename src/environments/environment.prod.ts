e09e53dba73590f997ab8a36c04a322197513333
orshemesh <chikos2011@gmail.com>
2021-04-12 16:17:56 +0300

    deploay herkou fix #2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCHYJfOzQF6aAIVleqYjFYHqjQLTfSTUNE',
+    authDomain: 'todolist-29ee0.firebaseapp.com',
+    projectId: 'todolist-29ee0',
+    storageBucket: 'todolist-29ee0.appspot.com',
+    messagingSenderId: '1018886706466',
+    appId: '1:1018886706466:web:0da82a6dc7e7618da8817b',
+    measurementId: 'G-Z2N1S7NPHN'
+  }
 };
